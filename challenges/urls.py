b4f3759df4e0fb290b30d274304a86538f81f25c
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-10-22 16:52:13 +0300

    The URL Tag for Dynamic URLs

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path("",views.index,name="index"),
   path("<int:month>",views.monthly_challenge_by_number),
   path("<str:month>",views.monthly_challenge,name="month-challenge"),
+  
 ]
 
